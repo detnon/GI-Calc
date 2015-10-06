dab41738dd4ab00b5ea5790f29cb241fb1ceedb7
sam.detnon <sam.detnon@headlondon.com>
2015-10-06 11:24:19 +0100

    Everything lives in Main Activity for now.
    - It works.

--- app/src/main/java/samdetnon/gi_calc/MainActivity.java
+++ app/src/main/java/samdetnon/gi_calc/MainActivity.java
@@ -4,8 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,13 +14,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final TextView firstValue = (TextView) findViewById(R.id.firstValue);
+        final TextView secondValue = (TextView) findViewById(R.id.secondValue);
         final TextView resultValue = (TextView) findViewById(R.id.result_value);
         final Button calculateButton = (Button) findViewById(R.id.calculateButton);
+
+
+
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                CalculateLoad newLoad = new CalculateLoad();
-                resultValue.setText(newLoad.toString());
+
+                float indexValue = Integer.parseInt(firstValue.getText().toString());
+                float carbsInGrams = Integer.parseInt(secondValue.getText().toString());
+
+                float multiplied = indexValue * carbsInGrams;
+                float result = multiplied / 100;
+
+                String resultString = Float.toString(result);
+
+                resultValue.setText(resultString);
             }
         };
         calculateButton.setOnClickListener(listener);
